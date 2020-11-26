34a8ffa3858a4e4dc413228469bc0e81d1da6f16
Dmitriy Telepnev <dmitry.telepnev@rock-west.com>
2020-11-26 11:49:28 +0700

    fixed Metrics comment

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,7 +2,7 @@ package metrics
 
 import "github.com/DmitryTelepnev/mongo-command-monitor/event"
 
-// Metric collector interface
+// Metrics collector interface
 type Metrics interface {
 	CollectStartedQuery(query *event.Query)
 	CollectFinishedQuery(query *event.Query, status string)
